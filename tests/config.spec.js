1e64924987985ebc3eb618977e7e5bd687e09558
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-04-07 16:53:40 -0500

    Fixed path to config.js file

--- tests/config.spec.js
+++ tests/config.spec.js
@@ -1,6 +1,6 @@
 describe("Configuration setup", function(){
 	it("should load local configurations", function(next){
-		var config = require('./config')('local');
+		var config = require('../config.js')('local');
 		expect(config.mode).toBe('local');
 		next();
 	});
