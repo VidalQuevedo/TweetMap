8ef3ae33683601d9b1bd7928dc8e4f62ace7a474
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-04-07 16:15:07 -0500

    replace jade with hbs

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var hbs = require('hbs');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.set('html', hbs.__express);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
