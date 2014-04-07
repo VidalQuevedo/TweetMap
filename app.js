8d4c859ffeb571369edaf379030099dac0d46868
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-04-07 17:19:58 -0500

    Make sure connection to db has started before starting server

--- app.js
+++ app.js
@@ -3,16 +3,15 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
-var hbs = require('hbs');
+ var express = require('express');
+ var routes = require('./routes');
+ var http = require('http');
+ var path = require('path');
+ var hbs = require('hbs');
 
-var app = express();
+ var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine ('html', hbs.__express);
@@ -26,11 +25,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+
+// Connect to MongoDB and start up HTTP server
+
+MongoClient.connect('mongodb://' + config.host + ':' + config.port + '/' + config.db, function(err, db){
+
+	if (err) { 
+		console.log("Couldn't connect to database. Please check your configuration in config.js");
+	} else {
+		http.createServer(app).listen(config.port, function(){
+			console.log('Express server listening on port ' + config.port);
+		});
+	}
 });
+
+
+
