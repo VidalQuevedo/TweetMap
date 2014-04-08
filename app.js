35df51a9d6b832f6cdac46f6268b631a285edd65
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2014-04-07 21:29:59 -0500

    Fix call to config in app.js

--- app.js
+++ app.js
@@ -5,9 +5,11 @@
 
  var express = require('express');
  var routes = require('./routes');
+ var config = require('./config.js')();
  var http = require('http');
  var path = require('path');
  var hbs = require('hbs');
+ var MongoClient = require('mongodb').MongoClient;
 
  var app = express();
 
@@ -33,16 +35,13 @@ app.get('/', routes.index);
 
 // Connect to MongoDB and start up HTTP server
 
-MongoClient.connect('mongodb://' + config.host + ':' + config.port + '/' + config.db, function(err, db){
+MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/' + config.mongo.db, function(err, db){
 
 	if (err) { 
-		console.log("Couldn't connect to database. Please check your configuration in config.js");
+		console.log("Couldn't connect to database. Please check your configuration in config.js (" + err + ")");
 	} else {
-		http.createServer(app).listen(config.port, function(){
-			console.log('Express server listening on port ' + config.port);
+		http.createServer(app).listen(config.mongo.port, function(){
+			console.log('Express server listening on port ' + config.mongo.port);
 		});
 	}
 });
-
-
-
